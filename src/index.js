6f17779c1fd7789b7199f460a6b830bf8ad6b333
MoetassemCh <103120209+MoetassemCh@users.noreply.github.com>
2022-10-31 20:17:11 +0200

    sidebar navabar

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {registerLicense} from "@syncfusion/ej2-base"
+import {ContextProvider} from "./contexts/ContextProvider"
+
+ registerLicense(
+   "ORg4AjUWIQA/Gnt2VVhjQlFac11JXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0VgWX5fcXZVT2BdVEQ="
+ );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+     <App />
+    </ContextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
